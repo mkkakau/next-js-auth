84e00a1305ca5b3a36c7f85f1b1cbe7bb64e694c
Melanie Akau <mkkakau@gmail.com>
2024-04-09 18:46:50 -1000

    improve auth

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./styles/globals.css";
 import Navbar from "@/components/Navbar";
+import { AuthContextProvider } from "./stores/authContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,8 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        <AuthContextProvider>
+          <Navbar />
+          {children}
+        </AuthContextProvider>
       </body>
     </html>
   );
